b562d21d0392f64d81b6596349c02c5c202b780d
mshajkarami <mshajkarami.dev@gmail.com>
2025-02-06 18:37:59 +0330

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+
+        maven {
+            url = uri("https://maven.myket.ir")
+        }
+
+
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Math Base Game"
+include(":app")
